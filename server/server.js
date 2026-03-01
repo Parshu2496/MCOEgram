dff019c0546d8e621aaba13c371699da1ad6a967
parshu2496 <prashantnrwd@gmail.com>
2026-03-01 18:37:26 +0530

    renameing

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -45,7 +45,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
   },
 });
--- server/sockets.js
+++ server/sockets.js
@@ -7,7 +7,7 @@ const Message = require("./Models/Message");
 module.exports = function (server) {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.FRONTEND_URL,
       methods: ["GET", "POST"],
     },
   });
